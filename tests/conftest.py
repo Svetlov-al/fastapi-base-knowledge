ae62d8657e0ccbf9bdd7bdbfeb2cf38bf016ec7c
Svetlov-al <s3t1qs@gmail.com>
2023-06-03 21:15:07 +0400

    rebuild

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,10 +7,10 @@ from app.database import get_db, Base
 from app.oauth2 import create_access_token
 from app import models
 import pytest
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:iyu1vap8@localhost:5432/fastapi_test"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:iyu1vap8@localhost:5432/fastapi_test"
 
 
-# SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
